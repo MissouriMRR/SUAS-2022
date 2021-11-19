aba2cc63a2f8901c660f4c64e7abac6b4b7185e4
pieperm <michaelepieper@gmail.com>
2021-11-18 19:40:25 -0600

    Add action for static type checks with mypy

--- vision/Object Detection/object_bounding_box.py
+++ vision/Object Detection/object_bounding_box.py
@@ -7,7 +7,7 @@ import argparse
 import random
 
 
-def get_bounding_image(img: np.array, contour: np.array) -> np.array:
+def get_bounding_image(img: np.ndarray, contour: np.ndarray) -> np.ndarray:
     """
     Calculate the bounding box with the given contour and then crop the given image.
 
