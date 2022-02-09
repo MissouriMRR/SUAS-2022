c85e2769729335f97d14e54d5e2a536fba8f882f
Cameron Falls <camfalls0201@gmail.com>
2022-02-08 18:59:54 -0600

    Auto-Formatting

---
+++ vision/Object Detection/standard_obj_text.py
@@ -0,0 +1,26 @@
+"""
+Functions related to detecting the text of standard objects.
+"""
+
+
+def detect_text():
+    """
+    Detect text within an image.
+
+    Returns location and character. ## TODO: Documentation to be updated later
+    """
+    return
+
+
+def get_text_color():
+    """
+    Detect the color of the text.
+    """
+    return
+
+
+if __name__ == "__main__":
+    """
+    Driver for testing text detection and classification functions.
+    """
+    pass
