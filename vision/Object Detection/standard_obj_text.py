Fixed angle calculation

vision/Object Detection/standard_obj_text.py
@@ -30,6 +30,17 @@ POSSIBLE_COLORS: Dict[str, npt.NDArray[np.int64]] = {
     "ORANGE": np.array([[24, 255, 255], [10, 50, 70]]),
 }
 
+POSSIBLE_ORIENTATIONS: List[str] = [
+    "N",
+    "NE",
+    "E",
+    "SE",
+    "S",
+    "SW",
+    "W",
+    "NW",
+]
+
 
 class TextCharacteristics:
     """
@@ -53,8 +64,7 @@ class TextCharacteristics:
         self._color: Optional[str] = None
 
         # related to text orientation
-        # NOTE: not implemented
-        # self.orientation: Optional[str] = None
+        self.orientation: Optional[str] = None
 
     @property
     def img(self) -> npt.NDArray[np.uint8]:
@@ -478,12 +488,46 @@ class TextCharacteristics:
 
         return self._color
 
-    def get_orientation(self) -> Optional[str]:
+    def get_orientation(
+        self, drone_degree: float, obj_bounds: BoundingBox, char_bounds: BoundingBox
+    ) -> Optional[str]:
         """
         Get the orientation of the text.
-        NOTE: Not yet implemeneted.
+
+        Parameters
+        ----------
+        drone_degree : float
+            rotation angle of the drone relative to north
+        obj_bounds : BoundingBox
+            bounds of the standard object on which the text is contained
+        char_bounds : BoundingBox
+            bounds of the text on the cropped standard object
+
+        Returns
+        -------
+        orientation : Optional[str]
+            the Cardinal/Intermediate direction as a 1 or 2 letter string
         """
-        raise NotImplementedError("Orientation not implemented.")
+        dir_width: float = 45.0  # width of each orientation's range
+
+        self.orientation = None
+
+        # angle from the x-axis (east)
+        total_angle: float = obj_bounds.get_rotation_angle() + char_bounds.get_rotation_angle()
+
+        # add drone angle
+        total_angle += drone_degree
+
+        # account for north wrapping around
+        total_angle += dir_width / 2
+        if total_angle >= 360:
+            total_angle -= 360
+
+        # get which range angle falls into
+        octant: int = int(total_angle // dir_width)
+        orientation = POSSIBLE_ORIENTATIONS[octant]
+
+        return orientation
 
 
 # Driver for testing text detection and classification functions.
@@ -521,5 +565,11 @@ if __name__ == "__main__":
     detected_chars: Tuple[
         Optional[str], Optional[str], Optional[str]
     ] = detector.get_text_characteristics(test_bounds)
+
+    # Code for testing orientation
+    # drone_angle = 0
+    # odlc_bounds = BoundingBox(((0, 0), (100, 0), (100, 100), (0, 100)), obj_type=ObjectType.TEXT)
+    # text_bounds = BoundingBox(((0, 0), (10, 0), (10, 10), (0, 10)), obj_type=ObjectType.TEXT)
+    # print(detector.get_orientation(drone_angle, odlc_bounds, text_bounds))
 
     print("The following character was found in the image:", detected_chars)