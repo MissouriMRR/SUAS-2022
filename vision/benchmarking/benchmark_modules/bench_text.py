b08969a629ab00f2aaeb863001bd25d5d19eab17
Cameron Falls <camfalls0201@gmail.com>
2022-04-19 18:58:57 -0500

    Flesh out benchmark structure

--- vision/benchmarking/benchmark_modules/bench_text.py
+++ vision/benchmarking/benchmark_modules/bench_text.py
@@ -1,3 +1,17 @@
 """
 Module for benchmarking algorithms related to ODLC text.
 """
+
+from vision.benchmarking.common.template import Benchmark
+
+
+class BenchmarkText(Benchmark):
+    """
+    Benchmarks algorithms related to ODLC text.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def benchmark_times(self):
+        raise NotImplementedError("Benchmark not implemented")
--- vision/benchmarking/common/template.py
+++ vision/benchmarking/common/template.py
@@ -2,3 +2,17 @@
 Template for benchmarking modules. Benchmark modules should
 be a subclass Benchmark.
 """
+
+from typing import List
+
+
+class Benchmark:
+    """
+    Template for benchmark modules.
+    """
+
+    def __init__(self):
+        self._times: List[float] = []
+
+    def benchmark_times(self):
+        raise NotImplementedError("Calling base class.")
