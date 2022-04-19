8c766a23f98b06c715653df4d5b2cd06bb50e1af
Cameron Falls <camfalls0201@gmail.com>
2022-04-19 18:40:19 -0500

    Add enums and functions

--- vision/benchmarking/common/benchmarks.py
+++ vision/benchmarking/common/benchmarks.py
@@ -5,6 +5,15 @@ Enumeration for storing the types of benchmarks.
 from enum import Enum
 
 
+class RunType(Enum):
+    """
+    The type of image to run the benchmark on.
+    """
+
+    BLANK_IMGS: str = "blank_imgs"
+    IMG_SET: str = "img_set"
+
+
 class BenchmarkType(Enum):
     """
     The benchmark that is being run.
--- vision/benchmarking/run_benchmark.py
+++ vision/benchmarking/run_benchmark.py
@@ -3,6 +3,40 @@ Central file for running benchmarks of vision code. Benchmarks
 are used to score timing and accuracy.
 """
 
+from typing_extensions import runtime
+
+from vision.benchmarking.common.benchmarks import BenchmarkType
+from vision.benchmarking.common.benchmarks import RunType
+
+
+def run_blank():
+    """
+    Run on a certain number of blank images.
+    """
+    raise NotImplementedError("Function not yet implemented.")
+
+
+def run_set():
+    """
+    Run on a set of images in a folder.
+    """
+    raise NotImplementedError("Function not yet implemented.")
+
+
+def run_bench(benchmark_name: str, run_type: str):
+    """
+    Run the specified benchmark using a set of images or generated blank images.
+
+    Parameters
+    ----------
+    benchmark_name : str
+        the name of the benchmark
+    run_type : str
+        the type of images to run on (either a set of images or blank images)
+    """
+    raise NotImplementedError("Function not yet implemented.")
+
+
 # Driver for running vision benchmarks
 if __name__ == "__main__":
     import argparse
@@ -18,9 +52,27 @@ if __name__ == "__main__":
         help="Name of the benchmark. Required argument.",
     )
 
+    parser.add_argument(
+        "-r",
+        "--run_type",
+        type=str,
+        help="Type of images to run on. Required argument.",
+    )
+
     args: argparse.Namespace = parser.parse_args()
 
     # no benchmark name specified, cannot continue
     if not args.benchmark_name:
         raise RuntimeError("No benchmark specified.")
-    benchmark_name: str = args.benchmark_name
+    if args.benchmark_name not in [item.value for item in BenchmarkType]:
+        raise RuntimeError("An invalid benchmark was given. Check spelling and try again.")
+    arg_benchmark_name: str = args.benchmark_name
+
+    # no run type specified, cannot continue
+    if not args.run_type:
+        raise RuntimeError("No run type specified.")
+    if args.run_type not in [item.value for item in RunType]:
+        raise RuntimeError("An invalid run type was given. Check spelling and try again.")
+    arg_run_type: str = args.run_type
+
+    run_bench(benchmark_name=arg_benchmark_name, run_type=arg_run_type)
