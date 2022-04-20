5a0f62d75e14e9fe66b3886b0b36a96160a103d7
Cameron Falls <camfalls0201@gmail.com>
2022-04-19 19:53:49 -0500

    initial algorithm

--- vision/Object Detection/emergent_detection.py
+++ vision/Object Detection/emergent_detection.py
@@ -2,15 +2,17 @@
 Functions relating to detection of the emergent object.
 """
 
+from typing import List, Tuple
+
 import cv2
 
 import numpy as np
 import numpy.typing as npt
 
-from vision.common.bounding_box import BoundingBox
+from vision.common.bounding_box import BoundingBox, ObjectType
 
 
-def get_emg_bounds(img: npt.NDArray[np.uint8]) -> BoundingBox:
+def get_emg_bounds(img: npt.NDArray[np.uint8]) -> List[BoundingBox]:
     """
     Gets the bounds of the emergent object in an image.
 
@@ -19,7 +21,30 @@ def get_emg_bounds(img: npt.NDArray[np.uint8]) -> BoundingBox:
     img : npt.NDArray[np.uint8]
         the image to find the emergent object in
     """
-    raise NotImplementedError("Function not implemented yet.")
+    # Initialize HOG object
+    hog: cv2.HOGDescriptor = cv2.HOGDescriptor()  # the hog
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+    # Find boudning boxes
+    win_stride: Tuple[int, int] = (4, 4)  # sliding window step size x, y
+    padding: Tuple[int, int] = (8, 8)  # x, y padding
+    scale: float = 1.05  # determines size of image pyramid
+
+    rects: List[Tuple[int, int, int, int]]
+    rects, _ = hog.detectMultiScale(img, winStride=win_stride, padding=padding, scale=scale)
+
+    # Convert to bounding boxes
+    detected_people: List[BoundingBox] = []
+    for x, y, width, height in rects:
+        bounds: Tuple[Tuple[int, int], Tuple[int, int], Tuple[int, int], Tuple[int, int]] = (
+            (x, y),
+            (x + width, y),
+            (x + width, y + height),
+            (x, y + height),
+        )
+        box = BoundingBox(vertices=bounds, obj_type=ObjectType.EMG_OBJECT)
+        detected_people.append(box)
+    return detected_people
 
 
 # Driver for testing emergent object functions
@@ -27,7 +52,7 @@ if __name__ == "__main__":
     import argparse
 
     parser: argparse.ArgumentParser = argparse.ArgumentParser(
-        description="Runs emergent object characteristics algorithms. Must specify a file."
+        description="Runs emergent object detection algorithms. Must specify a file."
     )
 
     parser.add_argument(
