74a890b33000cec301ca08a89461b66cdf7b594e
mat <madprzez@gmail.com>
2022-04-26 17:34:30 -0500

    test

--- vision/pipline.py
+++ vision/pipline.py
@@ -5,26 +5,54 @@ Takes information from the camera and returns interop output json file.
 import os
 import sys
 import json
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, wait
 import time
+import cv2
+import numpy as np
 
-def wait_function(x, y):
-    print('Task(', x,'multiply', y, ') started')
-    print('Task(', x,'multiply', y, ') completed')
-    return x * y
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+stitch_dir = parent_dir + "\mapping"
+image_dir = parent_dir + "\images"
 
-with ThreadPoolExecutor() as executor: #change max_workers to 2 and see the results
-    future = executor.submit(wait_function, 3, 4)
-    future2 = executor.submit(wait_function, 8, 8)
-    future3 = executor.submit(wait_function, 12, 12)
-    while True:
-        if(future.running()):
-            print("Task 1 running")
-        if(future2.running()):
-            print("Task 2 running")
-        if(future3.running()):
-            print("Task 3 running")
+from mapping.image_stitcher import Stitcher
 
-        if(future.done() and future2.done() and future3.done()):
-            print(future.result(), future2.result()), future3.done()
-            break
+def run_modules(image):
+    print("start task")
+    time.sleep(2)
+    print("doing task")
+
+def img_processor(stitch, image_list):
+    with ProcessPoolExecutor() as executor:
+        img_proccess = [executor.submit(run_modules, image) for image in image_list]
+        img_stitch =  executor.submit(stitch.multiple_image_stitch())
+        wait(img_proccess)
+        while True:
+            if(img_stitch.done()):
+                print("All done")
+                break
+
+if __name__ == '__main__':
+    stitch = Stitcher()
+    stitch.image_path = stitch_dir
+
+    image_list = []
+
+    if not os.listdir(stitch_dir):
+        raise IndexError("No Images")
+
+    for file in os.listdir(stitch_dir):
+        if file.endswith(".jpg"):
+            image_list.append(cv2.imread(os.path.join(stitch_dir, file)))
+
+    start = time.time()
+    img_processor(stitch, image_list)
+    end = time.time()
+    print("MULTIPROC", end-start)
+
+    image = np.array([])
+    start = time.time()
+    for _ in range(4):
+        run_modules(image)
+        stitch.multiple_image_stitch()
+    end = time.time()
+    print("NORMAL", end-start)
