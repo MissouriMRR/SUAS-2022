b1d8eafbd7e776f4bf87efee22c616c389a5a873
Sean-Walsh-496 <seantheprofessional1@gmail.com>
2022-09-27 18:30:19 -0500

    now places ones where the cells are

--- flight/odlc/cell_map.py
+++ flight/odlc/cell_map.py
@@ -1,4 +1,6 @@
-from typing import Collection, Iterable, List
+from typing import Collection, Iterable, List, Dict
+from xml.dom import EMPTY_PREFIX
+from helper import coord_conversion
 # constants to check:
 # P(see | height && r)
 # Vision radius
@@ -29,13 +31,36 @@ class CellMap:
                 elif points[i][dim[0]] > dim[1][1]: #biggest x | y
                     dim[1][1] = points[i][dim[0]]
 
-        print(x_bounds, y_bounds)
-        return x_bounds, y_bounds
+        return {'x': x_bounds, 'y': y_bounds}
 
-    def create_map(self, points: Collection[Iterable[int]]) -> List[List]:
-        bounds = self.get_bounds(points)
+    def __init_map_shape(self, bounds: Dict[chr, Iterable[int]]) -> List[List]:
+        """
+        returns an empty array with a shape given the bounds.
+        """
+        return_list = []
+        for _ in range(bounds['y'][0], bounds['y'][1] + 1):
+            row = []
+            for _ in range(bounds['x'][0], bounds['x'][1] + 1):
+                row.append(0)
+            return_list.append(row)
+    
+        return return_list
 
-        return "deez"
+    def __init_map(self, points: Collection[Iterable[int]]) -> List[List]:
+        """
+        Creates the list of the map and populates it with cells
+        """
+        empty = self.__init_map_shape(self.get_bounds(points))
+        for point in points:
+            new_point = coord_conversion(point, (len(empty[0]), len(empty)))
+            empty[new_point[0]][new_point[1]] = 1
+        
+
+        return empty
+
+    def display(self):
+        for i in range(len(self.data)):
+            print(self.data[i])
 
     def __init__(self, points: Collection[Iterable[int]]):
         """
@@ -44,7 +69,7 @@ class CellMap:
         points: Collection[Iterable[int]]
         a collection of x,y coordinates to define the map
         """
-        self.data = self.create_map(points)
+        self.data = self.__init_map(points)
 
 
 if __name__ == "__main__":
@@ -52,6 +77,6 @@ if __name__ == "__main__":
         (0, 2), (1, 2), (2, 2), (3, 2),
         (0, 1), (1, 1), (2, 1),
         (0, 0), (1, 0), (2, 0),
-        (0, -1)
     ]
     test_map = CellMap(test_points)
+    test_map.display()
