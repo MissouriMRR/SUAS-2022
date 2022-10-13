05ae4e3ed4b672a3d18144b497e5172ece43d94c
Sean-Walsh-496 <seantheprofessional1@gmail.com>
2022-10-13 18:26:50 -0500

    created a seeker object that searchers the probability map

---
+++ flight/odlc/seeker.py
@@ -0,0 +1,41 @@
+from typing import Tuple, List
+import math
+class Seeker:
+    def __init__(self, start: Tuple[int], find_prob: float, view: int, prob_map: object) -> object:
+        self.index = start
+        self.find_prob = find_prob #probability of finding object when cell is in view
+        self.view = view
+        self.prob_map = prob_map
+
+    def get_in_view(self) -> List[Tuple[int]]:
+        """
+        returns a list of displacement vectors that can be seen from the drone
+        based upon its range.
+        """
+        dist = lambda p1, p2: math.sqrt((p1[0] - p2[0])** 2 + (p1[1] - p2[1])**2)
+        view_list = []
+
+        for i in range(self.view * 2 + 1):
+            for j in range(self.view * 2 + 1):
+                if dist((i, j), (self.view, self.view)) <= self.view:
+                    view_list.append((i, j))
+        return view_list
+        
+
+    def move(self, disp_vec: Tuple[int]) -> None:
+        try:
+            new_pos = [self.index[0] + disp_vec[0], self.index[1] + disp_vec[1]]
+
+            if self.prob_map[new_pos[0]][new_pos[1]].is_valid:
+                self.prob_map.update_probs(new_pos, self)
+            else:
+                pass
+        except:
+            pass 
+
+
+if __name__ == "__main__":
+    s = Seeker((0, 0), 0.9, 5, [])
+    space = s.get_in_view()
+    for cell in space:
+        print(cell)
