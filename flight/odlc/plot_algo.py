d4e2722be7808f529736b2600274871d732c7de5
Sean-Walsh-496 <seantheprofessional1@gmail.com>
2022-10-20 18:38:22 -0500

    Defined the function

---
+++ flight/odlc/plot_algo.py
@@ -0,0 +1,6 @@
+def probability_catcher(seeker: object, prob_map: object, n: int = 10):
+    """
+    looks n moves into the future and tries to find a path with the largest
+    probability per point.
+    """
+    
